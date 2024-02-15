361e6b074aa5bf33d618142dcc91208d9ba667b1
Nikhil Singh <nikhils@Nikhils-MacBook-Pro.local>
2024-02-15 10:31:49 +0530

    initalized isValidClaim function

--- tambola.kt
+++ tambola.kt
@@ -2,4 +2,10 @@ fun main() {
   val board = listOf(setOf(), setOf(), setOf())
   val announcements = listOf()
   val claim = ""
+
+  result = isValidClaim(board, announcements, claim)
+}
+
+fun isValidClaim(board: List<Set<Int>>, announcements: List<Int>, claim: String): Boolean {
+  
 }
