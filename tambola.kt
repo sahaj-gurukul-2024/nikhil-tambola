b3bb137adf477ef5063fd36ee206c076c70844ab
Nikhil Singh <nikhils@Nikhils-MacBook-Pro.local>
2024-02-15 10:41:22 +0530

    almost done

--- tambola.kt
+++ tambola.kt
@@ -1,9 +1,15 @@
 fun main() {
-  val board = listOf(setOf(), setOf(), setOf())
-  val announcements = listOf()
-  val claim = ""
+  val board = listOf(setOf(4, 16, 48, 63, 76), setOf(7, 23, 38), setOf())
+  val announcements = listOf(90, 4, 46, 63, 89, 16, 76, 78)
+  val claim = "top"
 
   result = isValidClaim(board, announcements, claim)
+
+  if (result == true) {
+    println("Accepted")
+  } else {
+    println("Rejected")
+  }
 }
 
 fun isValidClaim(board: List<Set<Int>>, announcements: List<Int>, claim: String): Boolean {
