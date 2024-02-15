d706b554b4d8f5e84136938d741863ec3ed7b48a
Nikhil Singh <nikhils@Nikhils-MacBook-Pro.local>
2024-02-15 10:34:40 +0530

    partial completion

--- tambola.kt
+++ tambola.kt
@@ -7,5 +7,22 @@ fun main() {
 }
 
 fun isValidClaim(board: List<Set<Int>>, announcements: List<Int>, claim: String): Boolean {
+  var topMatches = 0
+  var bottomMatches = 0
+  var overallMatches = 0
+
+  for (i in 0..announcements.size-2) {
+    if (announcements[i] in board[0]) {
+      topMatches += 1
+      overallMatches += 1
+    }
+    if (announcements[i] in board[1]) {
+      overallMatches += 1
+    }
+    if (announcements[i] in board[2]) {
+      bottomMatches += 1
+      overallMatches += 1
+    }
+  }
   
 }
